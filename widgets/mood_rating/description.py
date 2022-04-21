2874dee254b8e38ef5cd74c5505ba77851511570
Жора <gorik@MacBook-Pro-Zora.local>
2022-04-22 00:08:03 +0300

    loc and doc commit

--- widgets/mood_rating/description.py
+++ widgets/mood_rating/description.py
@@ -4,37 +4,121 @@ import pkgutil
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.slider import MDSlider
 from kivy_garden.graph import MeshLinePlot
+import locale
+import gettext
 
+"""
+.. module:: mood_rating
+   :synopsis: 
+       Module for mood rating
+
+.. moduleauthor:: SuccsessContent <github.com/SuccsessContent>
+
+
+"""
+
+def setlocale(loc=None):
+    if loc is None:
+        l = locale.getdefaultlocale()[0]
+    else:
+        l = loc
+    lc = gettext.translation('mood_rating', localedir='locales', languages=[l])
+    lc.install()
+    return lc.gettext, lc.ngettext
 
 class MoodRating(MDBoxLayout):
-    """Screen with mood rating slider and chart."""
+    """
+
+    **MoodRating**
+
+    Screen with mood rating slider and chart.
+
+
+    """
+    def __init__(self, **kwargs):
+        """
+
+        **MoodRating.__init__**
+
+        Init this class.
+
+
+        """
+        super().__init__(**kwargs)
+        _, ngettext = setlocale()
+        self.rate = _("Remove last rate")
+        self.x_label = _("Mood rate")
 
     @staticmethod
     def get_descritpion() -> str:
-        """Return content of description kv."""
+        """
+
+        **MoodRating.get_descritpion**
+
+        Return content of description kv.
+
+
+        """
         return pkgutil.get_data(__name__, "description.kv").decode("utf-8")
 
 
 class MoodSlider(MDSlider):
-    """Slider for mood rating."""
+    """
+
+    **MoodSlider**
+
+    Slider for mood rating.
+
+
+    """
+
 
     def __init__(self, **kwargs):
-        """Init basics and register events."""
+        """
+
+        **MoodSlider.get_descritpion**
+
+        Init basics and register events.
+
+
+        """
+
         self.register_event_type('on_slider_release')
         super().__init__(**kwargs)
 
     def on_slider_release(self):
-        """Do nothing. We should declare func for each event."""
+        """
+
+        **MoodSlider.on_slider_release**
+
+        Do nothing. We should declare func for each event.
+
+
+        """
 
     def on_touch_up(self, touch):
-        """Dispatch event on_slider_release if only touch up triggered by this class."""
+        """
+
+        **MoodSlider.on_touch_up**
+
+        Dispatch event on_slider_release if only touch up triggered by this class.
+
+
+        """
         super().on_touch_up(touch)
         if touch.grab_current == self:
             self.dispatch('on_slider_release')
 
     @staticmethod
     def set_callback(value, storage, graph_box) -> None:
-        """Update storage and redraw chart."""
+        """
+
+        **MoodSlider.set_callback**
+
+        Update storage and redraw chart.
+
+
+        """
         mood_history = storage.get("mood_history")
         mood_history["values"].append(value)
         storage.put("mood_history", **mood_history)
