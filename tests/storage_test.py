f937cae0dd1bf35e009f3ad4f365c9331de7ed5d
Жора <gorik@MacBook-Pro-Zora.local>
2022-06-12 14:28:56 +0300

    style(storage_test.py) add "with"

--- tests/storage_test.py
+++ tests/storage_test.py
@@ -8,60 +8,60 @@ from custom_storage import Storage
 
 def test_storage_inited():
     """Test storage set_inited() and inited() methods."""
-    file = tempfile.NamedTemporaryFile(suffix='.dict', delete=False)
-    storage = Storage(file.name)
+    with tempfile.NamedTemporaryFile(suffix='.dict', delete=False) as file:
+        storage = Storage(file.name)
 
-    try:
-        assert not storage.inited()
-        storage.set_inited()
-        assert storage.inited()
-    finally:
-        file.close()
-        os.unlink(file.name)
+        try:
+            assert not storage.inited()
+            storage.set_inited()
+            assert storage.inited()
+        finally:
+            file.close()
+            os.unlink(file.name)
 
 def test_storage_put_get():
     """Test storage put() and get() methods."""
-    file = tempfile.NamedTemporaryFile(suffix='.dict', delete=False)
-    storage = Storage(file.name)
-    storage.set_inited()
+    with tempfile.NamedTemporaryFile(suffix='.dict', delete=False) as file:
+        storage = Storage(file.name)
+        storage.set_inited()
 
-    day1 = date.today()
-    day2 = date.today() + timedelta(days=1)
-    day3 = date.today() + timedelta(days=-1)
+        day1 = date.today()
+        day2 = date.today() + timedelta(days=1)
+        day3 = date.today() + timedelta(days=-1)
 
-    storage.put('mood', day1, 5, "Great Day!")
-    storage.put('mood', day2, 3, "Normal Day")
-    storage.put('mood', day3, 2, "Bad sleep")
+        storage.put('mood', day1, 5, "Great Day!")
+        storage.put('mood', day2, 3, "Normal Day")
+        storage.put('mood', day3, 2, "Bad sleep")
 
-    try:
-        assert storage.get_message('mood', day1) == "Great Day!"
-        assert storage.get_message('mood', day2) == "Normal Day"
-        assert storage.get_message('mood', day3) == "Bad sleep"
+        try:
+            assert storage.get_message('mood', day1) == "Great Day!"
+            assert storage.get_message('mood', day2) == "Normal Day"
+            assert storage.get_message('mood', day3) == "Bad sleep"
 
-        assert storage.get_value('mood', day1) == 5
-        assert storage.get_value('mood', day2) == 3
-        assert storage.get_value('mood', day3) == 2
-    finally:
-        file.close()
-        os.unlink(file.name)
+            assert storage.get_value('mood', day1) == 5
+            assert storage.get_value('mood', day2) == 3
+            assert storage.get_value('mood', day3) == 2
+        finally:
+            file.close()
+            os.unlink(file.name)
 
 def test_storage_put_today():
     """Test storage put_today() method."""
-    file = tempfile.NamedTemporaryFile(suffix='.dict', delete=False)
-    storage = Storage(file.name)
-    storage.set_inited()
+    with tempfile.NamedTemporaryFile(suffix='.dict', delete=False) as file:
+        storage = Storage(file.name)
+        storage.set_inited()
 
-    day = date.today()
-    storage.put_today('mood', 5, "Great Day!")
+        day = date.today()
+        storage.put_today('mood', 5, "Great Day!")
 
-    try:
-        assert storage.get_message('mood', day) == "Great Day!"
-        assert storage.get_value('mood', day) == 5
+        try:
+            assert storage.get_message('mood', day) == "Great Day!"
+            assert storage.get_value('mood', day) == 5
 
-        storage.put_today('mood', 3, "Regular Day")
+            storage.put_today('mood', 3, "Regular Day")
 
-        assert storage.get_message('mood', day) == "Regular Day"
-        assert storage.get_value('mood', day) == 3
-    finally:
-        file.close()
-        os.unlink(file.name)
+            assert storage.get_message('mood', day) == "Regular Day"
+            assert storage.get_value('mood', day) == 3
+        finally:
+            file.close()
+            os.unlink(file.name)
