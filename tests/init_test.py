499867a15ae6a07045cd71b24d0c79fb57dd9573
Жора <gorik@MacBook-Pro-Zora.local>
2022-06-12 10:23:59 +0300

    added init test

---
+++ tests/init_test.py
@@ -0,0 +1,14 @@
+"""Tests for init card"""
+
+from main import MainApp
+
+
+def test_init_card():
+    """Check if init works properly"""
+    
+    app = MainApp()
+    assert not app.storage.inited()
+    app.run()
+    assert app.storage.inited()
+    
+    
